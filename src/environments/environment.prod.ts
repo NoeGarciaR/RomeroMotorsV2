57dd6e647823f652a47382e693b9b1b19f9e0b62
Noe Garcia <jnoe.garciar@gmail.com>
2020-08-22 11:40:33 -0500

    Dando styles principales a las cards de resultados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   baseURL: "https://romero-motors-api.herokuapp.com/api",
+  baseURL_UPL: 'https://romero-motors-api.herokuapp.com/uploads',
   keyCrypto: "123345"
 };
