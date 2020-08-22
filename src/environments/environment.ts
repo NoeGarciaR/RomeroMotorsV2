96eb02fea8739c5dd613cb33db9433757553fb02
Noe Garcia <jnoe.garciar@gmail.com>
2020-08-22 14:59:53 -0500

    Terminando diseño de cards de tire.item component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseURL: 'https://romero-motors-api.herokuapp.com/api',
   keyCrypto: '123345',
   baseURL_UPL: 'https://romero-motors-api.herokuapp.com/uploads'
