b01b9a506626fcb5eb1f33afc86e5b3710f0872b
Noe Garcia <jnoe.garciar@gmail.com>
2020-08-23 01:39:19 -0500

    Antes de bootstrap

--- src/app/pages/search/pages/filter-navbar-layout/filter-navbar-layout.component.ts
+++ src/app/pages/search/pages/filter-navbar-layout/filter-navbar-layout.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-navbar-layout.component.scss']
 })
 export class FilterNavbarLayoutComponent implements OnInit {
+  private click = 1;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ocultView() {
+    if (this.click === 1){
+      document.getElementById("options").style.height = "100%";
+      this.click = this.click + 1;
+    } else {
+      document.getElementById("options").style.height = "0px";
+      this.click = 1;
+    }
+  }
 }
